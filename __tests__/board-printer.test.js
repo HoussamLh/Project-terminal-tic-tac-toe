b76017b6e80198ae87fffb070972090eb0859e2e
Samiuk <lahlahhoussam80@gmail.com>
2025-08-07 00:50:23 +0100

    - add board-printer functions and passing tests

--- __tests__/board-printer.test.js
+++ __tests__/board-printer.test.js
@@ -1,6 +1,21 @@
 import { checkIfNoMovesLeft } from "../board-printer";
 
-test("checkIfNoMovesLeft should return true if there are no moves left", () => {
-  const board = [];
-  expect(checkIfNoMovesLeft(board)).toBe(true);
+describe("checkIfNoMovesLeft", () => {
+  test("returns true when the board is full (no '_' present)", () => {
+    const board = [
+      ['X', 'O', 'X'],
+      ['O', 'X', 'O'],
+      ['O', 'X', 'O']
+    ];
+    expect(checkIfNoMovesLeft(board)).toBe(true);
+  });
+
+  test("returns false when the board still has empty slots ('_')", () => {
+    const board = [
+      ['X', '_', 'X'],
+      ['O', 'X', 'O'],
+      ['O', 'X', 'O']
+    ];
+    expect(checkIfNoMovesLeft(board)).toBe(false);
+  });
 });
